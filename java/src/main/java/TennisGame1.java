f8b1e2de635b8c44309c519255e918228a17c698
Wallyson Ruan Alexandrino Ferreira <wallyson.ruan@MacBook-Pro-de-Wallyson.local>
2023-02-20 23:09:08 -0300

    REFACTOR: Map instead of switch

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,14 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private Map<Integer, String> scoreText = new HashMap<>();
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+
+        scoreText.put(0, "Love");
+        scoreText.put(1, "Fifteen");
+        scoreText.put(2, "Thirty");
+        scoreText.put(3, "Forty");
     }
 
     public void wonPoint(String playerName) {
@@ -20,55 +28,37 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        int tempScore = 0;
+        if (m_score1 == m_score2) {
+            switch (m_score1) {
                 case 0:
-                        score = "Love-All";
+                    score = "Love-All";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score = "Fifteen-All";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score = "Thirty-All";
                     break;
                 default:
-                        score = "Deuce";
+                    score = "Deuce";
                     break;
-                
+
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            int minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = m_score1;
+                else {
+                    score += "-";
+                    tempScore = m_score2;
                 }
+                score += scoreText.get(tempScore);
             }
         }
         return score;
